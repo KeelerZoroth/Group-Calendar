95a58d84f639c377fdabe284fe5277461109b890
Brian Barber <brian.tom.barber@gmail.com>
2024-11-21 21:46:37 -0500

    added calendarpage, navbar, and css to support

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,4 +1,32 @@
 
-const Navbar = () => {return <div></div>}
+import React, { useState } from 'react';
+import '../navbar.css';
 
-export default Navbar
+const Navbar: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginLogout = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">Group Calendar</div>
+      <ul className="navbar-links">
+        <li>
+          <a href="/calendar">Calendar</a>
+        </li>
+        <li>
+          <a href="/new-user">New User</a>
+        </li>
+        <li>
+          <button className="navbar-button" onClick={handleLoginLogout}>
+            {isLoggedIn ? 'Logout' : 'Login'}
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
